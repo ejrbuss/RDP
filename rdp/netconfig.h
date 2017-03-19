33d9ed0b2c307942ed6553797d560551831e48be
Eric Buss <ebuss@ualberta.ca>
2017-03-19 15:07:46 -0700

    autogenerated commit

--- rdp/netconfig.h
+++ rdp/netconfig.h
@@ -7,8 +7,8 @@
 #define SOCK_BUFFER_SIZE 2000
 #define ADDR_SIZE        200
 #define TIMEOUT          5
-#define MAXIMUM_TIMEOUTS 5
-#define MAXIMUM_RESETS   5
+#define MAXIMUM_TIMEOUTS 10
+#define MAXIMUM_RESETS   10
 #define WINDOW_SIZE      10
 
 // Events
