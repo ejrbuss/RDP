67800f0cdfccd7a059a4e14fc895064d2c97a05e
Eric Buss <ebuss@ualberta.ca>
2017-03-18 19:46:22 -0700

    autogenerated commit

--- rdp/netconfig.h
+++ rdp/netconfig.h
@@ -1,6 +1,7 @@
 #ifndef RDP_NOTCONFIG_HEADER
 #define RDP_NETCONFIG_HEADER
 
+// Net Constants
 #define SOCK_BUFFER_SIZE 2000
 #define ADDR_SIZE        200
 #define TIMEOUT          500
@@ -8,6 +9,7 @@
 #define MAXIMUM_RESETS   5
 #define WINDOW_SIZE      10
 
+// Events
 #define event_SYN        0
 #define event_FIN        1
 #define event_DAT        2
@@ -16,15 +18,26 @@
 #define event_bad_packet 5
 #define event_timeout    6
 
-// Stat names
-typedef enum stat_t {
-    stat_start_time,   stat_end_time,
-    stat_sent_bytes,   stat_sent_bytes_unique,   stat_recieved_bytes, stat_recieved_bytes_unique,
-    stat_sent_DAT,     stat_sent_DAT_unique,     stat_recieved_DAT,   stat_recieved_DAT_unique,
-    stat_sent_SYN,     stat_sent_FIN,            stat_sent_RST,       stat_sent_ACK,
-    stat_recieved_SYN, stat_recieved_FIN,        stat_recieved_RST,   stat_recieved_ACK,
-    stat_length
-};
+// Stat indexes
+#define stat_start_time            0
+#define stat_end_time              1
+#define stat_sent_bytes            2
+#define stat_sent_bytes_unique     3
+#define stat_recieved_bytes        4
+#define stat_recieved_bytes_unique 5
+#define stat_sent_DAT              6
+#define stat_sent_DAT_unique       7
+#define stat_recieved_DAT          8
+#define stat_recieved_DAT_unique   9
+#define stat_sent_SYN              10
+#define stat_sent_FIN              11
+#define stat_sent_RST              12
+#define stat_sent_ACK              14
+#define stat_recieved_SYN          15
+#define stat_recieved_FIN          16
+#define stat_recieved_RST          17
+#define stat_recieved_ACK          18
+#define stat_length                19
 
 extern void rdp_open_source_socket(const char* ip, const char* port);
 extern void rdp_open_destination_socket(const char* ip, const char* port);
