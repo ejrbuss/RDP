73555d0fff432d966ae372a018c7e06d12616ff3
Eric Buss <ebuss@ualberta.ca>
2017-03-18 19:39:12 -0700

    autogenerated commit

--- rdp/netconfig.h
+++ rdp/netconfig.h
@@ -9,17 +9,10 @@
 #define WINDOW_SIZE      10
 
 // Event names
-typedef enum event_t { event_SYN, event_FIN, event_DAT, event_RST, event_ACK, event_bad_packet, event_timeout };
+extern enum event_t;
 
 // Stat names
-typedef enum stat_t {
-    stat_start_time,   stat_end_time,
-    stat_sent_bytes,   stat_sent_bytes_unique,   stat_recieved_bytes, stat_recieved_bytes_unique,
-    stat_sent_DAT,     stat_sent_DAT_unique,     stat_recieved_DAT,   stat_recieved_DAT_unique,
-    stat_sent_SYN,     stat_sent_FIN,            stat_sent_RST,       stat_sent_ACK,
-    stat_recieved_SYN, stat_recieved_FIN,        stat_recieved_RST,   stat_recieved_ACK,
-    stat_length
-};
+extern enum stat_t;
 
 extern void rdp_open_source_socket(const char* ip, const char* port);
 extern void rdp_open_destination_socket(const char* ip, const char* port);
