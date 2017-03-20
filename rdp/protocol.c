3d819421bf39d81b6bfd45624233e93af32b50a8
Eric Buss <ebuss@ualberta.ca>
2017-03-20 16:07:25 -0700

    autogenerated commit

--- rdp/protocol.c
+++ rdp/protocol.c
@@ -54,6 +54,7 @@ char* rdp_pack(
     const char* payload
 ) {
     // Get total packet size
+    rdp_exit(EXIT_FAILURE, "size: %d", size);
 
     unint16_t checksum = rdp_checksum(flags, seq_ack_number, size, payload);
     char*      _magic_ = "CSC361";
