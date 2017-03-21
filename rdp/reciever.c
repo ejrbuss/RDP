0675f919af7e3a702c89358ccf29dfc7bb3c7889
Eric Buss <ebuss@ualberta.ca>
2017-03-20 22:38:12 -0700

    autogenerated commit

--- rdp/reciever.c
+++ rdp/reciever.c
@@ -169,10 +169,7 @@ void recieved_timeout() {
             connected, disconnecting
         );
         recieved_packets = 0;
-        timeout *= 1.5;
-        if(timeout > 128) {
-            timeout = 128;
-        }
+        timeout *= 1.1;
         if(timeout_count += (timeout / TIMEOUT) > MAXIMUM_TIMEOUTS) {
             rdp_close_sockets();
             rdp_exit(EXIT_FAILURE, "RDP transimmision failed as the connection timed out too many times.");
